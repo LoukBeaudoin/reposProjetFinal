e5db6d56967b5e43e6736a6c71ef87b25b89fb5c
MeallanM <meallan.milot.01@edu.cegeptr.qc.ca>
2024-12-04 13:46:59 -0500

    Update Adherent.cs

--- ProjetFinal/ProjetFinal/Adherent.cs
+++ ProjetFinal/ProjetFinal/Adherent.cs
@@ -9,17 +9,18 @@ namespace ProjetFinal
 {
     internal class Adherent
     {
-        int noIdentification=0;
+        string noIdentification="";
         string nom="";
         string prenom="";
         string adresse="";
         DateTime dateNaissance = DateTime.Now;
+        DateTime date = DateTime.Now;
 
         public Adherent() { }
 
-        public Adherent(int noIdentification, string nom, string prenom, string adresse, DateTime dateNaissance)
+        public Adherent(string noIdentification, string nom, string prenom, string adresse, DateTime dateNaissance)
         {
-            this.noIdentification = noIdentification;
+            this.noIdentification = noIdString(nom, prenom, dateNaissance);
             this.nom = nom;
             this.prenom = prenom;
             this.adresse = adresse;
@@ -27,14 +28,33 @@ namespace ProjetFinal
 
 
 
-            //Avec la date : Calculer l’age de la personne.
+           
             /*noIdentification = initiales + année naissance + randomint 3 chiffres 
         (AT-2003-152)*/
             //Min 18 ans
             //Pour chaque : enregistrer la liste des séances d’activité qu’il participe
         }
 
-        public int NoIdentification
+        public string noIdString(string nom, string prenom, DateTime dateNaissance)
+        {
+            string initiales = prenom.Substring(0,1) + nom.Substring(0,1);
+            int annéeNaissance = dateNaissance.Year;
+            Random random = new Random();
+            int randomDigits = random.Next(1, 1000);
+            string formattedRandom = randomDigits.ToString("D3");
+
+            return $"{initiales}-{annéeNaissance}-{formattedRandom}";
+        }
+
+        public double calculAge() 
+        {
+            TimeSpan nbJours = date - dateNaissance;
+            double age = (nbJours.Days) / 365.25;
+            return age; 
+        }
+
+
+        public string NoIdentification
         {
             get { return noIdentification; }
             set { noIdentification = value; }
