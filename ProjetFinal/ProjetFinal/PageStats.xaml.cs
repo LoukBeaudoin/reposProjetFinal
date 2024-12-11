1f72e3814945d0beba6e8f92bcda5310bdf82b0e
LoukBeaudoin <louka.beaudoin.01@edu.cegeptr.qc.ca>
2024-12-10 19:14:52 -0500

    pagestats xaml xaml.cs singleton

--- ProjetFinal/ProjetFinal/PageStats.xaml.cs
+++ ProjetFinal/ProjetFinal/PageStats.xaml.cs
@@ -7,6 +7,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,9 +25,82 @@ namespace ProjetFinal
     /// </summary>
     public sealed partial class PageStats : Page
     {
+        public ObservableCollection<KeyValuePair<string, int>> AdherentActivite { get; set; }
+        public ObservableCollection<KeyValuePair<string, int>> ActiviteNote { get; set; }
         public PageStats()
         {
             this.InitializeComponent();
+            Statistique();
+        }
+
+        private void Statistique()
+        {
+            //Total Adherent
+            var totalAdherent = SingletonBD.getInstance().getTotalAdherent();
+
+            nbTotalAdherent.Text = $"{totalAdherent}";
+
+            //Total Activite
+            var totalActivite = SingletonBD.getInstance().getTotalActivite();
+
+            nbTotalActivite.Text = $"{totalAdherent}";
+
+            //Nombre adherent par activités
+            var adherentCategorie = SingletonBD.getInstance().getNbAdherentActivite();
+            AdherentActivite = new ObservableCollection<KeyValuePair<string, int>>(adherentCategorie);
+            this.DataContext = this;
+
+            foreach (var activites in adherentCategorie)
+            {
+                TextBlock adherentText = new TextBlock
+                {
+                    Text = $"{activites.Key} : ",
+                    FontSize = 20,
+                };
+                TextBlock activiteResultat = new TextBlock
+                {
+                    Text = $"{activites.Value}",
+                    FontSize = 20,
+                };
+                stk_nbAdherentActivite.Children.Add(adherentText);
+                stk_nbAdherentActiviteResultat.Children.Add(activiteResultat);
+            }
+
+            //Adherent avec le plus de seance 
+            var adherentPlusSeance = SingletonBD.getInstance().getAdherentMaxSeance();
+            tbx_adherentPlusSeance.Text = $"{adherentPlusSeance}";
+
+            //Age moyen des adhérents
+            var ageMoyenAdherent = SingletonBD.getInstance().getAgeMoyenAdherent();
+
+            moyenneAge.Text = $"{ageMoyenAdherent}";
+
+            //Note moyenne des activités
+            var noteMoyenneActivite = SingletonBD.getInstance().getNoteMoyenneActivites();
+            ActiviteNote = new ObservableCollection<KeyValuePair<string, int>>(noteMoyenneActivite);
+            this.DataContext = this;
+
+            foreach (var activites in noteMoyenneActivite)
+            {
+                TextBlock activiteText = new TextBlock
+                {
+                    Text = $"{activites.Key} : ",
+                    FontSize = 20,
+                };
+                TextBlock noteMoyenne = new TextBlock
+                {
+                    Text = $"{activites.Value}",
+                    FontSize = 20,
+                };
+                stk_moyenneNoteActivite.Children.Add(activiteText);
+                stk_moyenneNoteActiviteResultat.Children.Add(noteMoyenne);
+            }
+
+            var totalInscription = SingletonBD.getInstance().getTotalInscription();
+
+            nbTotalInscription.Text = $"{totalInscription}";
+
+
         }
     }
 }
--- ProjetFinal/ProjetFinal/SingletonBD.cs
+++ ProjetFinal/ProjetFinal/SingletonBD.cs
@@ -15,7 +15,7 @@ namespace ProjetFinal
         static SingletonBD instance = null;
         public SingletonBD()
         {
-            con = new MySqlConnection("Server=cours.cegep3r.info;Database=420345ri_gr00002_2387284-louka-beaudoin;Uid=2387284;Pwd=2387284;");
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2024_420335ri_eq3;Uid=2387284;Pwd=2387284;");
             //liste = new ObservableCollection<ObjetProduit>();
         }
 
@@ -26,40 +26,6 @@ namespace ProjetFinal
 
             return instance;
         }
-
-        //va avoir besoin de un pour administrateur parcourir la bd et regarder si est dedans test
-
-        //Get activité reste a modifier avec casse et colonne
-        public void getActivite()
-        {
-            //liste.Clear();
-
-
-            //MySqlCommand commande = new MySqlCommand();
-            //commande.Connection = con;
-            //commande.CommandText = "Select * from produits";
-            //con.Open();
-            //MySqlDataReader r = commande.ExecuteReader();
-
-            //while (r.Read())
-            //{
-            //    string nom = r["nom"].ToString();
-            //    double prix = Convert.ToDouble(r["prix"]);
-            //    string categorie = r["categorie"].ToString();
-
-            //    ObjetProduit texte = new ObjetProduit(nom, prix, categorie);
-
-            //    liste.Add(texte);
-            //}
-
-
-            //r.Close();
-            //con.Close();
-
-
-        }
-
-        //Get seance reste a modifier avec lcasse et colonne
         public void getSeances()
         {
             //liste.Clear();
@@ -89,76 +55,157 @@ namespace ProjetFinal
 
         }
 
-        //get total d'adhérent juste besoin de modifier pour adherent
+        //Nombre total d'adhérent
         public int getTotalAdherent()
-        {
-            //MySqlCommand commande = new MySqlCommand();
-            //commande.Connection = con;
-            //commande.CommandText = "SELECT count(nom) as total from produits";
-            //con.Open();
-            //MySqlDataReader r = commande.ExecuteReader();
-            //int totalproduit = 0;
-            //if (r.Read())
-            //{
-            //    totalproduit = Convert.ToInt32(r["total"].ToString());
-
-            //}
-
-            //r.Close();
-            //con.Close();
-            //return totalproduit;
-            return 1;
-
-
-        }
-
-        //get total d'activité juste besoin de modifier pour activite
-        public int getTotalActivite()
-        {
-            //MySqlCommand commande = new MySqlCommand();
-            //commande.Connection = con;
-            //commande.CommandText = "SELECT count(nom) as total from produits";
-            //con.Open();
-            //MySqlDataReader r = commande.ExecuteReader();
-            //int totalproduit = 0;
-            //if (r.Read())
-            //{
-            //    totalproduit = Convert.ToInt32(r["total"].ToString());
-
-            //}
-
-            //r.Close();
-            //con.Close();
-            //return totalproduit;
-            return 1;
-            
-
-
-        }
-
-
-        //reste juste a modifier pour adherent et activite la ses produit et categorie
-        public Dictionary<string, int> getNbAdherentActivite()
         {
             MySqlCommand commande = new MySqlCommand();
             commande.Connection = con;
-            commande.CommandText = "SELECT categorie, COUNT(*) as total FROM produits GROUP BY categorie";
-            Dictionary<string, int> produitCategorie = new Dictionary<string, int>();
+            commande.CommandText = "SELECT count(noIdentification) as total from adherents";
             con.Open();
             MySqlDataReader r = commande.ExecuteReader();
-            while (r.Read())
+            int totalAdherent = 0;
+            if (r.Read())
             {
-                string categorie = r["categorie"].ToString();
-                int totalproduit = Convert.ToInt32(r["total"].ToString());
-                produitCategorie[categorie] = totalproduit;
+                totalAdherent = Convert.ToInt32(r["total"].ToString());
+
             }
 
             r.Close();
             con.Close();
-            return produitCategorie;
+            return totalAdherent;
 
         }
 
+        //Nombre total d'activité
+        public int getTotalActivite()
+        {
+            MySqlCommand commande = new MySqlCommand();
+            commande.Connection = con;
+            commande.CommandText = "SELECT count(nom) as total from activites";
+            con.Open();
+            MySqlDataReader r = commande.ExecuteReader();
+            int totalActivite = 0;
+            if (r.Read())
+            {
+                totalActivite = Convert.ToInt32(r["total"].ToString());
+
+            }
+
+            r.Close();
+            con.Close();
+            return totalActivite;
+         
+        }
+
+
+        //Nombre d'adhérents par activités(séance)
+        public Dictionary<string, int> getNbAdherentActivite()
+        {
+            MySqlCommand commande = new MySqlCommand();
+            commande.Connection = con;
+            //commande.CommandText = "SELECT nom, COUNT(*) as total FROM adherent GROUP BY activite";
+            commande.CommandText = "select a.nom,count(*) as total from seances\r\nINNER JOIN a2024_420335ri_eq3.activites a on seances.idActivite = a.idActivite\r\ngroup by seances.idActivite";
+            Dictionary<string, int> adherentActivite = new Dictionary<string, int>();
+            con.Open();
+            MySqlDataReader r = commande.ExecuteReader();
+            while (r.Read())
+            {
+                string activites = r["nom"].ToString();
+                int totalAdherent = Convert.ToInt32(r["total"].ToString());
+                adherentActivite[activites] = totalAdherent;
+            }
+
+            r.Close();
+            con.Close();
+            return adherentActivite;
+
+        }
+
+
+        //L'adhérent avec le plus de séance
+        public string getAdherentMaxSeance()
+        {
+            MySqlCommand commande = new MySqlCommand();
+            commande.Connection = con;
+            commande.CommandText = "SELECT\r\nnoIdentificationAdherent\r\nFROM inscription\r\nGROUP BY noIdentificationAdherent\r\nORDER BY COUNT(*) DESC\r\nLIMIT 1";
+            con.Open();
+            MySqlDataReader r = commande.ExecuteReader();
+            string noId = "";
+            if (r.Read())
+            {
+                noId = r["noIdentificationAdherent"].ToString();
+
+            }
+
+            r.Close();
+            con.Close();
+            return noId;
+
+        }
+
+        //Age Moyen des Adhérents
+        public int getAgeMoyenAdherent()
+        {
+            MySqlCommand commande = new MySqlCommand();
+            commande.Connection = con;
+            commande.CommandText = "SELECT ROUND(AVG(age)) as moyenne FROM adherents";
+            con.Open();
+            MySqlDataReader r = commande.ExecuteReader();
+            int ageMoyen = 0;
+            if (r.Read())
+            {
+                ageMoyen = Convert.ToInt32(r["moyenne"].ToString());
+
+            }
+
+            r.Close();
+            con.Close();
+            return ageMoyen;
+
+        }
+
+        //Note Moyenne Activité
+        public Dictionary<string, int> getNoteMoyenneActivites()
+        {
+            MySqlCommand commande = new MySqlCommand();
+            commande.Connection = con;
+            commande.CommandText = "SELECT nom, ROUND(AVG(noteAppreciation)) as moyenne FROM inscription\r\nINNER JOIN a2024_420335ri_eq3.seances s on inscription.idSeance = s.idSeances\r\nINNER JOIN a2024_420335ri_eq3.activites a on s.idActivite = a.idActivite\r\nGROUP BY nom";
+            Dictionary<string, int> activiteNote = new Dictionary<string, int>();
+            con.Open();
+            MySqlDataReader r = commande.ExecuteReader();
+            while (r.Read())
+            {
+                string activites = r["nom"].ToString();
+                int noteMoyenne = Convert.ToInt32(r["moyenne"].ToString());
+                activiteNote[activites] = noteMoyenne;
+            }
+
+            r.Close();
+            con.Close();
+            return activiteNote;
+
+        }
+
+        //Total inscription
+        public int getTotalInscription()
+        {
+            MySqlCommand commande = new MySqlCommand();
+            commande.Connection = con;
+            commande.CommandText = "SELECT COUNT(noIdentificationAdherent) AS total FROM inscription";
+            con.Open();
+            MySqlDataReader r = commande.ExecuteReader();
+            int totalInscription = 0;
+            if (r.Read())
+            {
+                totalInscription = Convert.ToInt32(r["total"].ToString());
+
+            }
+
+            r.Close();
+            con.Close();
+            return totalInscription;
+
+        }
 
 
     }
